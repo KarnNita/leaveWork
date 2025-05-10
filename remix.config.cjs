430a63df588bc4188be64d9e5b17a7c5c8a9015a
Karnnitra Sukpanich <karnnitra.s@ku.th>
2025-05-11 00:53:58 +0700

    move : files

---
+++ remix.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  future: {
+    v3_routeConvention: true
+  },
+  ignoredRouteFiles: ["**/.*"],
+  server: false,
+  appDirectory: "app/",
+  assetsBuildDirectory: "public/build",
+  publicPath: "/leaveWork/build/",
+  serverBuildPath: "build/index.js"
+};
